7d7ae871449cd55771d6f95ac369a143c58ab448
Paul Cenir <paul.cenir03@gmail.com>
2017-10-13 12:44:41 +0800

    add carousel in facts page

--- js/script.js
+++ js/script.js
@@ -3,6 +3,11 @@ $(document).ready(function (e) {
     $("[data-toggle=popover]").popover({
        
     });
- 
+
+    $('[data-ride=carousel]').carousel({
+        ride:true,
+        interval: 12000
+    })
+
 
 });	
